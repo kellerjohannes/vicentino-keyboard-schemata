261c410682042290eb51edda9d88ca316f281d25
Johannes Keller <johannes.keller@mab-bs.ch>
2022-10-06 15:38:02 +0200

    working on interval scale, rescaled svg size

--- svg-generator.lisp
+++ svg-generator.lisp
@@ -16,12 +16,12 @@
                             :y1 (yp (first point-list))
                             :x2 (xp origin)
                             :y2 (yp origin)
-                            :stroke "black")))
+                            :stroke "gray")))
         (t (draw scene (:line :x1 (xp (first point-list))
                               :y1 (yp (first point-list))
                               :x2 (xp (second point-list))
                               :y2 (yp (second point-list))
-                              :stroke "black"))
+                              :stroke "gray"))
            (path-iterator (rest point-list) origin scene))))
 
 (defun lookup-label-id (id)
@@ -40,9 +40,8 @@
                  :stroke "black"
                  :fill "black"
                  :text-anchor "middle"
-                 :font-size 20
-                 :font-family "Times, serif"
-                 )
+                 :font-size 40
+                 :font-family "Times, serif")
       (lookup-label-id label-id))))
 
 (defun ratio->xpos (ratio frame-interval xpos-range)
@@ -52,97 +51,83 @@
 
 (defun create-hairpin (a b key-id scene)
   (let* ((x-center (/ (+ (xp a) (xp b)) 2))
-         (y-center (- (yp a) (- x-center (xp a))))
+         (y-center (- (yp a) (* 0.4 (- x-center (xp a)))))
          (line-center (ratio->xpos (id->ratio key-id) 16/1 *label-range*)))
     (draw scene (:circle :cx x-center
                          :cy y-center
-                         :r 5
-                         :fill "red"))
+                         :r 10
+                         :fill "black"))
     (draw scene (:line :x1 x-center
                        :y1 y-center
                        :x2 line-center
                        :y2 (+ y-center 20)
-                       :stroke "red"))
+                       :stroke "black"))
     (draw scene (:line :x1 line-center
                        :y1 (+ y-center 20)
                        :x2 line-center
-                       :y2 500
-                       :stroke "red"))))
+                       :y2 "100%"
+                       :stroke "black"))))
 
+(defun make-path (root modifier suffix)
+  (format nil "~a-~a.~a" root modifier suffix))
 
 (defun write-svg-file (path)
-  (setf *svg-scale-factor* 40)
-  (setf *svg-y-shift* 370)
-  (setf *svg-x-shift* 20)
-  (with-svg-to-file (scene 'svg-1.2-toplevel :width 2250 :height 400)
-      (path :if-exists :supersede :if-does-not-exist :create)
-    ;; (draw scene (:circle :cx 200 :cy 150 :r 50) :fill "red")
-    (draw scene (:rect :x 0 :y 0 :width "100%" :height "100%"))
-    (let ((vertices (generate-vertices)))
-      (setf *label-range* '(nil . nil))
-      (dolist (row vertices)
-        (dolist (key-shape row)
-          (path-iterator key-shape (first key-shape) scene)
-          (output-svg-label (first key-shape)
+  "Path without suffix."
+  (setf *svg-scale-factor* 90)
+  (setf *svg-y-shift* 1000)
+  (setf *svg-x-shift* 50)
+  (let ((svg-width 6000)
+        (svg-height 1700))
+    (with-svg-to-file (scene 'svg-1.2-toplevel
+                             :width svg-width
+                             :height svg-height)
+        ((make-path path "keyboard" "svg")
+         :if-exists :supersede
+         :if-does-not-exist :create)
+      ;; (draw scene (:circle :cx 200 :cy 150 :r 50) :fill "red")
+      (draw scene (:rect :x 0 :y 0 :width "100%" :height "100%" :fill "white"))
+      (let ((vertices (generate-vertices)))
+        (setf *label-range* '(nil . nil))
+        (dolist (row vertices)
+          (dolist (key-shape row)
+            (path-iterator key-shape (first key-shape) scene)
+            (output-svg-label (first key-shape)
+                              (first (last key-shape))
+                              (third (first key-shape))
+                              scene)))
+        ;; needs to be separate because `output-svg-label' sets `*label-range*' first.
+        (dolist (row vertices)
+          (dolist (key-shape row)
+            (create-hairpin (first key-shape)
                             (first (last key-shape))
                             (third (first key-shape))
-                            scene)))
-      ;; needs to be separate because `output-svg-label' sets `*label-range*' first.
-      (dolist (row vertices)
-        (dolist (key-shape row)
-          (create-hairpin (first key-shape)
-                          (first (last key-shape))
-                          (third (first key-shape))
-                          scene)))
-      (loop for interval in *intervals-lantica*
-            for i from 1
-            do (let ((center (list (ratio->xpos (second interval) 16/1 *label-range*)
-                                   20)))
-                 (draw scene (:circle :cx (first center)
-                                      :cy (second center)
-                                      :r 5
-                                      :fill "blue")))))))
-
-
-;; copy-paste from tex-generator, as reference material
-
-(defun output-draw-command (a b)
-  (format t "~&\\draw[thick] (~s,~s) -- (~s,~s);" (first a) (second a) (first b) (second b)))
-
-(defun output-label-command (a b label-text)
-  (let ((avg (/ (+ (first a) (first b)) 2)))
-    (format t "~&\\node at (~s,~s) {\\Large{~a}};"
-            avg
-            (+ (- avg (first a)) (second a))
-            label-text)))
-
-(defun generate-pikz-code (vertices)
-  (dolist (row vertices)
-    (dolist (key-shape row)
-      (format t "~&% key number ~s:" (third (first key-shape)))
-      (do ((origin (first key-shape))
-           (point-list key-shape (rest point-list)))
-          ((null (rest point-list)) (output-draw-command (first point-list) origin))
-        (output-draw-command (first point-list) (second point-list)))
-      (output-label-command (first key-shape)
-                            (first (last key-shape))
-                            (cdr (assoc (third (first key-shape)) *dict-key-labels*))))))
-
-(defun read-string-from-file (pathname &key (buffer-size 4096) (element-type 'character))
-  "Return the contents of PATHNAME as a string."
-  (with-open-file (file-stream pathname)
-    (with-output-to-string (datum)
-      (let ((buffer (make-array buffer-size :element-type element-type)))
-        (loop for bytes-read = (read-sequence buffer file-stream)
-              do (write-sequence buffer datum :start 0 :end bytes-read)
-              while (= bytes-read buffer-size))))))
-
-(defun write-tex-file (path)
-  (with-open-file (file path
-                        :direction :output
-                        :if-exists :overwrite
-                        :if-does-not-exist :create)
-    (let ((*standard-output* file))
-      (format t "~a" (read-string-from-file "latex-template-preamble.tex"))
-      (generate-pikz-code (generate-vertices))
-      (format t "~a" (read-string-from-file "latex-template-closing.tex")))))
+                            scene)))))
+    (with-svg-to-file (scene 'svg-1.2-toplevel :width svg-width :height svg-height)
+        ((make-path path "intervals" "svg")
+         :if-exists :supersede
+         :if-does-not-exist :create)
+      (let ((padding 30))
+        (draw scene (:rect :x 0 :y 0 :width "100%" :height "100%" :fill "white"))
+        (loop for interval in *intervals-lantica*
+              for i from 1
+              do (let ((center (ratio->xpos (second interval) 16/1 *label-range*))
+                       (origin (ratio->xpos 1/1 16/1 *label-range*))
+                       (ypos (+ (* i padding) 20)))
+                   (draw scene (:line :x1 center :y1 0
+                                      :x2 center :y2 "100%"
+                                      :stroke (if (= i 1) "black" "gray")
+                                      :stroke-width (if (= i 1) 3 1)))
+                   (draw scene (:circle :cx origin
+                                        :cy ypos
+                                        :r 4
+                                        :fill "black"))
+                   (draw scene (:circle :cx center
+                                        :cy ypos
+                                        :r 4
+                                        :fill "black"))
+                   (draw scene (:line :x1 origin :y1 ypos
+                                      :x2 center :y2 ypos
+                                      :stroke "black"
+                                      :stroke-width 3))
+                   (text scene (:x (+ center 15) :y (+ ypos 6) :font-size 25 :font-family "Times, serif" :stroke "black" :fill "black")
+                     (first interval))))))))
